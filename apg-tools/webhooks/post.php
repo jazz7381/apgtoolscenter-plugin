4f7da818950d1de0f679932154f3200adf610bb8
rokumens <7381@asiapowergames.com>
2020-10-28 07:00:32 +0700

    update to v1.3.0

--- apg-tools/webhooks/post.php
+++ apg-tools/webhooks/post.php
@@ -2,32 +2,65 @@
 ini_set("memory_limit",'-1');
 ini_set('max_execution_time', 0);
 ini_set('auto_detect_line_endings', true);
+$response = [
+  'status'  => FALSE,
+  'message' => 'Failed to get response.'
+];
 try {
   if($post['token_auth'] == apg_config()->token_auth){
     $realPost = $post['post'];
-    unset($realPost['content']);
-    $imageBase64 = getImageHtml($post['post']['content']);
-    $attach_id = apg_base64_image_upload($realPost, $imageBase64);
-    $catId = wp_create_category($post['post']['category']);
-    // Create post object
-    $my_post = array(
-      'post_title'    => wp_strip_all_tags( $post['post']['title'] ),
-      'post_content'  => $post['post']['content'],
-      'post_status'   => 'publish',
-      'post_author'   => 1,
-      'post_category' => array($catId)
-    );
-    // Insert the post into the database
-    $postId = apg_wp_insert_post($my_post);
-
-    set_post_thumbnail($postId, $attach_id);
-    echo json_encode([
-      'status' => TRUE,
-      'message' => 'Success'
-    ]);
+    // common bulk post
+    if($realPost['type'] == 'common'){
+      unset($realPost['content']);
+      $imageBase64 = getImageHtml($post['post']['content']);
+      $attach_id = apg_base64_image_upload($realPost, $imageBase64);
+      $catId = wp_create_category($post['post']['category']);
+      // Create post object
+      $my_post = array(
+        'post_title'    => wp_strip_all_tags( $post['post']['title'] ),
+        'post_content'  => $post['post']['content'],
+        'post_status'   => 'publish',
+        'post_author'   => 1,
+        'post_category' => array($catId)
+      );
+      // Insert the post into the database
+      $postId = apg_wp_insert_post($my_post);
+      // set attachment thumbnail to post
+      set_post_thumbnail($postId, $attach_id);
+    }elseif($realPost['type'] == 'schedule'){// schedule post
+      // get image
+      $imageBase64  = $realPost['image'];
+      // set image as atachment of post
+      $attach_id    = apg_base64_image_upload($realPost, $imageBase64);
+      // get category
+      $arrayCatIds = [];
+      foreach($realPost['category'] as $value){
+        $arrayCatIds[] = wp_create_category($value);
+      }
+      // Create post object
+      $my_post = array(
+        'post_title'    => wp_strip_all_tags( $post['post']['title'] ),
+        'post_content'  => $post['post']['content'],
+        'post_status'   => 'publish',
+        'post_author'   => 1,
+        'post_category' => $arrayCatIds,
+        'tags_input'    => $realPost['tag'],
+      );
+      // Insert the post into the database
+      $postId = apg_wp_insert_post($my_post);
+      // set attachment thumbnail to post
+      set_post_thumbnail($postId, $attach_id);
+    }
+    $response['status']   = TRUE;
+    $response['message']  = 'Success';
   }else{
-    return http_response_code(403);
+    $response['status']   = FALSE;
+    $response['message']  = 'Token Invalid.';
   }
 } catch (\Throwable $th) {
-  file_put_contents('log.txt', $th);
+  $response['status']   = FALSE;
+  $response['message']  = 'Error : '.$th->getMessage();
+  file_put_contents('log.txt', $th->getMessage());
 }
+echo json_encode($response);
+exit;
